566a265717d76b4b3679c38b58d11937d0495070
Gong Zhang <zhanggong@me.com>
2023-03-11 23:23:36 +0800

    Remove space from package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "GPT3 Tokenizer",
+    name: "GPT3Tokenizer",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
